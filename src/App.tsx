48369158e6e093cc6cbc22ba4a33a6a57320064a
Piyush <yadavpiyush222@gmail.com>
2023-06-26 16:18:16 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import Sidebar from "./sidebar"
+
+ 
+ const App = () => {
+
+
+return (
+  <div className="flex flex-row ">
+      <Sidebar/>
+      <div className="flex justify-end">
+  <span className="text-black w-60vw">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi inventore soluta minus id itaque, explicabo officia doloremque repellendus fugiat ducimus optio voluptate eum! Praesentium autem nesciunt inventore aliquam laborum cumque non beatae nam quos cupiditate eum quo sint, itaque, id sit ipsa eaque modi quibusdam at, nihil esse. Eum quasi eveniet eligendi amet fugiat quo repudiandae deleniti praesentium optio at fugit perferendis beatae assumenda, deserunt, ipsum nesciunt, id ipsa mollitia sed reiciendis. Sed repudiandae provident quaerat debitis? Voluptatibus ipsa illum debitis dicta iste culpa cumque? Itaque numquam quae, voluptatum molestiae minima eum quos, maiores ad voluptatibus dolores nesciunt laudantium libero!\
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi inventore soluta minus id itaque, explicabo officia doloremque repellendus fugiat ducimus optio voluptate eum! Praesentium autem nesciuntiores ad voluptatibus dolores nesciunt laudantium liberoLorem ipsum dolor sit amet consectetur adipisicing elit. Quasi inventore soluta minus id itaque, explicabo officia doloremque repellendus fugiat ducimus optio voluptate eum! Praesentium autem nesciunt inventore aliquam laborum cumque non beatae nam quos cupiditate eum quo sint, itaque, id sit ipsa eaque modi quibusdam at, nihil esse. Eum quasi eveniet eligendi amet fugiat quo repudiandae deleniti praesentium optio at fugit perferendis beatae assumenda, deserunt, ipsum nesciunt, id ipsa mollitia sed reiciendis. Sed repudiandae provident quaerat debitis? Voluptatibus ipsa illum debitis dicta iste culpa cumque? Itaque numquam quae, voluptatum molestiae minima eum quos, maiores ad voluptatibus dolores nesciunt laudantium libero!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi inventore soluta minus id itaque, explicabo officia doloremque repellendus fugiat ducimus optio voluptate eum! Praesentium autem nesciunt inventore aliquam laborum cumque non beatae nam quos cupiditate eum quo sint, itaque, id sit ipsa eaque modi quibusdam at, nihil esse. Eum quasi eveniet eligendi amet fugiat quo repudiandae deleniti praesentium optio at fugit perferendis beatae assumenda, deserunt, ipsum nesciunt, id ipsa mollitia sed reiciendis. Sed repudiandae provident quaerat debitis? Voluptatibus ipsa illum debitis dicta iste culpa cumque? Itaque numquam quae, voluptatum molestiae minima eum quos, maiores ad voluptatibus dolores nesciunt laudantium libero!!
+    </p>
+  </span>
+</div>
+  </div>
+
+ 
+  
+
+)
+}
+export default App
---
+++ src/sidebar.tsx
@@ -0,0 +1,56 @@
+import  { useState } from 'react';
+import orgIcon from './icons/organizations.svg'
+import supportIcon from './icons/support.svg'
+import settingIcon from './icons/setting.svg'
+import teamIcon from './icons/team.svg'
+import dashboardIcon from './icons/dashboard.svg'
+import upgradeIcon from './icons/upgrade.svg'
+import './sidebar.css';
+
+const Sidebar = () => {
+  const [open, setOpen] = useState(true);
+
+
+
+
+  return (
+    <div className={`flex flex-col flex-shrink-0 items-center text-white mr-10 bg-black ${open? "w-full sm:w-[20rem]": "w-[5rem] "} h-[100vh] transition`}>
+
+        <div className={`flex ${open? "justify-end": "justify-center"} w-full`}>
+        <button className="px-2 pt-1" onClick={() => setOpen(!open)}>{open ? '<<<' : '>>>'}</button>
+        </div>
+        
+
+        <div className='w-full flex flex-col items-center'>
+             <h3 className=" text-2xl text-bold mb-12">{open? "Organization":""}</h3>
+             <div className={`main-menu flex flex-col w-full`}>
+                    <h4 className='text-[0.75rem] opacity-50 ml-8 pl-8'>{open?"MAIN MENU":""}</h4>
+                    <div className={`my-6 flex flex-col w-full ${open?"":"items-center"}`}>
+                      <button className={`hover:bg-white hover:bg-opacity-40 ${open? "ml-8 pl-8 w-3/4 py-2": "p-2"} mb-4 rounded-md flex items-center space-x-2`}><span><img src={orgIcon} alt="" /></span>{open? <span>Organization</span>:""}</button> 
+                      <button className={`hover:bg-white hover:bg-opacity-40 ${open? "ml-8 pl-8 w-3/4 py-2": "p-2"} mb-4 rounded-md flex items-center space-x-2`}><span><img src={dashboardIcon} alt="" /></span>{open?<span>Dashboard</span>:""}</button>
+
+                      <button className={`hover:bg-white hover:bg-opacity-40 ${open? "ml-8 pl-8 w-3/4 py-2": "p-2"} mb-4 rounded-md flex items-center space-x-2`}><span><img src={teamIcon} alt="" /></span>{open?<span>Team & Members</span>:""}</button>
+                      <button className={`hover:bg-white hover:bg-opacity-40 ${open? "ml-8 pl-8 w-3/4 py-2": "p-2"} mb-4 rounded-md flex items-center space-x-2`}><span><img src={upgradeIcon} alt="" /></span>{open?<span>Upgrade & Plans</span>:""}</button>
+                       
+                    </div>
+             </div>
+             <div className={`General flex flex-col w-full`}>
+                    <h4 className='text-[0.75rem] opacity-50 ml-8 pl-8'>{open?"General":""}</h4>
+                    <div className={`my-6 flex flex-col w-full ${open?"":"items-center"}`}>
+                      <button className={`hover:bg-white hover:bg-opacity-40 ${open? "ml-8 pl-8 w-3/4 py-2": "p-2"} mb-4 rounded-md flex items-center space-x-2`}><span><img src={supportIcon} alt="" /></span>{open?<span>Support</span>:""}</button>
+                      <button className={`hover:bg-white hover:bg-opacity-40 ${open? "ml-8 pl-8 w-3/4 py-2": "p-2"} mb-4 rounded-md flex items-center space-x-2`}><span><img src={settingIcon} alt="" /></span>{open?<span className='transition duration-300'>Settings</span>:""}</button>
+                    </div>
+                
+             </div>
+
+      </div>
+      
+        
+    </div>
+      
+
+  );
+};
+
+export default Sidebar;
+
